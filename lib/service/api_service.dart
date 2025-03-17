68d1070d3011f6ae96d000dc7f1507ec8682cc92
arakene <arakene_dev@naver.com>
2025-03-17 20:46:27 +0900

    feat: 웹툰 #6.4 fromJson

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,12 +1,26 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:test_flutter/widgets/models/webtoon_model.dart';
 
 class ApiSerivce {
   final baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final todayEndpoint = "today";
 
-  void getTodayToons() async {
+  Future<List<WebtoonModel>> getTodayToons() async {
+    final webtoonList = <WebtoonModel>[];
     final response = await http.get(Uri.parse("$baseUrl/$todayEndpoint"));
 
-    print(">>>>TEST ${response.body}");
+    if (response.statusCode == 200) {
+      final List<dynamic> list = jsonDecode(response.body);
+
+      for (final webtoon in list) {
+        webtoonList.add(WebtoonModel.fromJson(webtoon));
+      }
+    }
+
+    webtoonList.forEach((info) => print(info));
+
+    return webtoonList;
   }
 }
---
+++ lib/widgets/models/webtoon_model.dart
@@ -0,0 +1,19 @@
+class WebtoonModel {
+  final String title, thumbnail, id;
+
+  WebtoonModel({
+    required this.title,
+    required this.thumbnail,
+    required this.id,
+  });
+
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+    : title = json["title"],
+      thumbnail = json["thumb"],
+      id = json["id"];
+
+  @override
+  String toString() {
+    return 'WebtoonModel{title: $title, thumbnail: $thumbnail, id: $id}';
+  }
+}
