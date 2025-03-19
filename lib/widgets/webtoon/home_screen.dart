7b07cb85ebd274427d27efd9295cabd6309c3b49
arakene <arakene_dev@naver.com>
2025-03-19 21:44:41 +0900

    feat: 웹툰 #6.6 FutureBuilder

--- lib/widgets/webtoon/home_screen.dart
+++ lib/widgets/webtoon/home_screen.dart
@@ -2,23 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:test_flutter/service/api_service.dart';
 import 'package:test_flutter/widgets/models/webtoon_model.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiSerivce.getTodayToons();
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +21,20 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 2,
         centerTitle: true,
       ),
-      body: Container(child: TextButton(onPressed: () {}, child: Text("test"))),
+      /**
+       * Stateful위젯을 사용하지 않고 데이터를 호출해 state변경 감지 가능한 widget
+       * 내부적으로 stateful widget으로 future await을 대신 처리해줌
+       */
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text("Has Data");
+          } else {
+            return Text("Loading");
+          }
+        },
+      ),
     );
-  }
-
-  void waitForWebToons() async {
-    webtoons = await ApiSerivce.getTodayToons();
-    isLoading = false;
-    setState(() {});
   }
 }
