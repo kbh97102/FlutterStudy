f43c195577689064aefe671a6d6dc8c3b72f6cb2
arakene <arakene_dev@naver.com>
2025-03-22 21:07:27 +0900

    feat: 웹툰 #6.14 Detail Info

--- lib/widgets/models/webtoon_detail_model.dart
+++ lib/widgets/models/webtoon_detail_model.dart
@@ -1,22 +1,9 @@
 class WebtoonDetailModel {
-  final String title;
-  final String genre;
-  final String id;
-  final String age;
+  final String title, about, genre, age;
 
-  WebtoonDetailModel({
-    required this.title,
-    required this.genre,
-    required this.id,
-    required this.age,
-  });
-
-  factory WebtoonDetailModel.fromJson(Map<String, dynamic> json) {
-    return WebtoonDetailModel(
-      title: json['title'],
-      genre: json['genre'],
-      id: json['id'],
-      age: json['age'],
-    );
-  }
+  WebtoonDetailModel.fromJson(Map<String, dynamic> json)
+    : title = json['title'],
+      about = json['about'],
+      genre = json['genre'],
+      age = json['age'];
 }
--- lib/widgets/webtoon/detail_screen.dart
+++ lib/widgets/webtoon/detail_screen.dart
@@ -83,6 +83,32 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ],
           ),
+          const SizedBox(height: 25),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 15,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return CircularProgressIndicator();
+            },
+          ),
         ],
       ),
     );
