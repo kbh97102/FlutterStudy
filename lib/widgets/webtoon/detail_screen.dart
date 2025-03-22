14fbc1f4b09a78c4e1289c95690ea5158cd1b921
arakene <arakene_dev@naver.com>
2025-03-22 19:21:17 +0900

    feat: 웹툰 #6.10 Hero

--- lib/widgets/webtoon/detail_screen.dart
+++ lib/widgets/webtoon/detail_screen.dart
@@ -30,26 +30,33 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15,
-                      offset: Offset(10, 10),
-                      color: Colors.black.withValues(alpha: 0.2),
-                    ),
-                  ],
-                ),
-                child: Image.network(
-                  thumb,
-                  // 네이버에서 앱에서 실행하면 막아서 브라우저로 agent 변경
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                  },
+              /**
+               *  Hero는 하나의 이미지가 이동해서 보여지는 것처럼 애니메이션을 컨트롤해줌
+               *  태그 매칭
+               */
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: Offset(10, 10),
+                        color: Colors.black.withValues(alpha: 0.2),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(
+                    thumb,
+                    // 네이버에서 앱에서 실행하면 막아서 브라우저로 agent 변경
+                    headers: const {
+                      "User-Agent":
+                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    },
+                  ),
                 ),
               ),
             ],
--- lib/widgets/webtoon/webtoon_widget.dart
+++ lib/widgets/webtoon/webtoon_widget.dart
@@ -25,26 +25,29 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: Offset(10, 10),
-                  color: Colors.black.withValues(alpha: 0.2),
-                ),
-              ],
-            ),
-            child: Image.network(
-              thumb,
-              // 네이버에서 앱에서 실행하면 막아서 브라우저로 agent 변경
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withValues(alpha: 0.2),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                thumb,
+                // 네이버에서 앱에서 실행하면 막아서 브라우저로 agent 변경
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
 
