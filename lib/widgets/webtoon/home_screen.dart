a21b42fdb1b5bc89d059b6875f1c3cf732c44fd0
arakene <arakene_dev@naver.com>
2025-03-29 20:43:01 +0900

    feat: subcomposeLayout 궁금증 해결

--- lib/widgets/webtoon/home_screen.dart
+++ lib/widgets/webtoon/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_flutter/service/api_service.dart';
 import 'package:test_flutter/widgets/models/webtoon_model.dart';
-import 'package:test_flutter/widgets/webtoon/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -31,38 +30,34 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final data = snapshot.data ?? List.empty();
+            print(data);
             /**
              * ListView를 사용하면 주어진 데이터 모두를 한번에 그림
              * ListView.builder를 사용하면 보여지는 아이템만 그리고 안보이게된 것들은 메모리에서 제거함
              * seperated도 builder와 동일하게 로직탐
-             *
-             * Column 내부에서 LazyRow를 쓰면 height에 infinity 가 오면서 사이즈 측정 오류가 나옴
-             *  따라서 expanded를 통해서 남은 영역을 사용하도록하면 사이즈가 측정되며 에러 해결됨
              */
-            return Column(
-              children: [SizedBox(height: 50), Expanded(child: makeList(data))],
-            );
+            return WebtoonListView(data: data);
           }
           return const Center(child: CircularProgressIndicator());
         },
       ),
     );
   }
+}
 
-  ListView makeList(List<WebtoonModel> data) {
+class WebtoonListView extends StatelessWidget {
+  const WebtoonListView({super.key, required this.data});
+
+  final List<WebtoonModel> data;
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.separated(
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        final webtoon = data[index];
-        return Webtoon(
-          title: webtoon.title,
-          thumb: webtoon.thumbnail,
-          id: webtoon.id,
-        );
+        return Text(data[index].title);
       },
       itemCount: data.length,
-      separatorBuilder: (context, index) => SizedBox(width: 40),
+      separatorBuilder: (context, index) => SizedBox(height: 20),
     );
   }
 }
